3ad4969b2a4473ab8e54e82f6d02ea8d8ee24e79
KDavis006 <110576696+KDavis006@users.noreply.github.com>
2022-10-18 17:11:44 -0700

    js comments

--- Main.js
+++ Main.js
@@ -1,3 +1,4 @@
+// makes the navbar work while in a smaller screen by making it go away and come back due to the 3 lines
 function myFunction() {
   var x = document.getElementById("myLinks");
   if (x.style.display === "block") {
